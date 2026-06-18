f62ae565379181e671853f6f91ebff74aa72d14b
hugodraxler <basiliocopahugodraxler>
2026-06-18 18:59:43 -0400

    subiendo seguridad hugo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Seguridad from './pages/Seguridad'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        
+      <Seguridad />
     </>
-  )
+  );
 }
 
 export default App
