e4dddf20331f5992822943d19b7c35b606fe2227
alexander <ag1250780@gmail.com>
2026-06-23 18:56:46 -0400

    sibiendo admin-recursos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import AdminRecursos from './pages/Admin-Recursos'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        
+      <AdminRecursos />
     </>
   )
 }
