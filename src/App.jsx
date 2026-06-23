64f577316fa734158327c23c88d2516ee6a9c77b
Gustavo154716 <quispequispejesusgustavo@gmail.com>
2026-06-23 19:12:34 -0400

    subiendo unidadadmin rama gustavo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import UnidadAdmin from './pages/UnidadAdmin'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        
+    <UnidadAdmin />
     </>
   )
 }
